e9f35ffcdb0544b818766b3efed2d917b641f8aa
Clem Hepburn <clemhepburn@gmail.com>
2021-08-11 14:32:12 -0700

    artist pagination

--- src/components/home/Home/Home.js
+++ src/components/home/Home/Home.js
@@ -1,25 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ArtistList from '../../display/ArtistList';
 import Loading from '../../common/Loading';
 import Search from '../../controls/Search';
 import { fetchArtists } from '../../../services/artistApi.js';
 
 export default function Home() {
+  const [page, setPage] = useState(0);
   const [artists, setArtists] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearch] = useState('');
+  useEffect(() => {
+    if (searchTerm !== '') {
+      setLoading(true);
+      fetchArtists(page, searchTerm).then(setArtists).finally(setLoading(false));
+    }
+  }, [page]);
+
 
   const handleSearch = (searchTerm) => {
     setLoading(true);
-    fetchArtists(0, searchTerm).then(setArtists).finally(() => setLoading(false));
-  
+    fetchArtists(page, searchTerm).then(setArtists).finally(() => setLoading(false));
+    console.log(searchTerm,);
+    setSearch(searchTerm);
   };
 
-  if(loading) return <Loading />;
+  const handlePageChange = (newPage) => {
+    setPage(prevPage => prevPage + newPage);
+    // setLoading(true);
+    // console.log(newPage);
+    // fetchArtists(newPage, searchTerm).then(setArtists).finally(() => setLoading(false));
+  };
+
+
+  if (loading) return <Loading />;
   return (
     <div>
       <p>Search artists</p>
       <Search onSearch={handleSearch} />
       <ArtistList artists={artists} />
+      <button onClick={() => handlePageChange(-25)} disabled={page <= 0}>←</button>
+      <button onClick={() => handlePageChange(25)}>→</button>
     </div>
   );
 }
