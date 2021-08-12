98052d0451f479b56de70041129724e6d39b2321
Gabriel Simek <gabrielsimek@outlook.com>
2021-08-12 11:35:21 -0700

    add https

--- src/services/artistApi.js
+++ src/services/artistApi.js
@@ -1,5 +1,5 @@
 export const fetchArtists = async (page, searchTerm) => {
-  const res = await fetch(`http://musicbrainz.org/ws/2/artist?query=${searchTerm}&fmt=json&limit=25&offset=${page}`);
+  const res = await fetch(`https://musicbrainz.org/ws/2/artist?query=${searchTerm}&fmt=json&limit=25&offset=${page}`);
   const { artists } = await res.json();
   return artists;
 };
--- src/services/titlesApi.js
+++ src/services/titlesApi.js
@@ -1,5 +1,5 @@
 export const fetchTitles = async (id) => {
-  const res = await fetch(`http://musicbrainz.org/ws/2/recording?release=${id}&fmt=json`);
+  const res = await fetch(`https://musicbrainz.org/ws/2/recording?release=${id}&fmt=json`);
   const { recordings } = await res.json();
   return recordings;
 };
