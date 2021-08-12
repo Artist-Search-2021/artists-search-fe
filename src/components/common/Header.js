d3edeca8a48c70f6cfa79d874d08b10d358d91a9
Clem Hepburn <clemhepburn@gmail.com>
2021-08-12 09:43:22 -0700

    css-in-js Header styling

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderBottom: [2, 'solid', 'black'],
+    padding: '1rem',
+  },
+  h1: {
+    margin: 0,
+    fontSize: '1.8rem',
+  },
+  link: {
+    color: 'black',
+    fontWeight: 600,
+  }
+
+})
 
 export default function Header() {
+  const classes = useStyles();
   return (
-    <header>
-      <h1>Music App</h1>
+    <header className={classes.header}>
+      <h1 className={classes.h1}>Music App</h1>
       <nav>
-        <NavLink to="/">Home</NavLink>
+        <NavLink className={classes.link} to="/">Home</NavLink>
       </nav>
     </header>
   );
